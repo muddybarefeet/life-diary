02082900388b38b013b8fe7c8abc2ef91839cdd8
Anna Rogers <2muddybarefeet@gmail.com>
2016-08-03 06:35:02 -0700

    feat: add code to get device username

--- lifeDiary/homeViewController.swift
+++ lifeDiary/homeViewController.swift
@@ -17,12 +17,15 @@ class homeViewController: UIViewController {
     //2. set up core data
     //3. set up slider
     //4. make and pass the core data model to the next view controller
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("view controller did load")
-        
-        
+        //test exact username returned may wan to slice of some of it
+        let user = UIDevice.currentDevice().name
+        greeting.text = "Hello " + user + "!"
+        print("user", user)
     }
     
     @IBAction func more(sender: AnyObject) {
